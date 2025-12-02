1d22d79f9f06738a0a6574a899dec35c5a6869e5
deeksha <deekshavernekar11@gmail.com>
2025-12-02 10:41:28 +0530

    save

---
+++ test_product.py
@@ -0,0 +1,9 @@
+from product import product_details
+def test_product_details():
+    expected_output = (
+        "Product Name: Laptop\n"
+        "Product ID: P456\n"
+        "Price: 1200\n"
+        "Quantity: 10\n"
+    )
+    assert product_details("Laptop","P456",1200,10) == expected_output
